ff8a2316c4474d8439bd590427b5ff015fc9737c
Siddharth Singh <iamsidz89@gmail.com>
2021-08-21 22:31:24 +0530

    ui done

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,15 +1,22 @@
+import Card from "../UI/Card";
+import classes from './AddUser.module.css'
+import Button from '../UI/Button'
 const AddUser = (props) => {
   const addUserHandler = (e) => {
     e.preventDefault();
   };
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">UserName</label>
-      <input type="text" id="UserName"></input>
-      <label htmlFor="age">Age in years</label>
-      <input type="number" id="Age"></input>
-      <button type="submit"> Add User</button>
-    </form>
+    <Card className = {classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">UserName</label>
+        <input type="text" id="UserName"></input>
+        <label htmlFor="age">Age in years</label>
+        <input type="number" id="Age"></input>
+        {/* <button type="submit"> Add User</button> */}
+        <Button>Add user</Button>
+      </form>
+
+    </Card>
   );
 };
-export default AddUser ;
+export default AddUser;
